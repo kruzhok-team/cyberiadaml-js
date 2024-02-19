7ff131c2da8aea1fa3a5adfabb44e41709cc4c92
L140-beep <kartoshka0302@mail.ru>
2024-02-19 16:46:10 +0800

    test parsing ArduinoUno schema

--- src/parseCGML.test.ts
+++ src/parseCGML.test.ts
@@ -236,3 +236,203 @@ exit/
     })
 }
 );
+
+test("test parsing ArduinoUno schema", () => {
+  expect(parseCGML(`<?xml version="1.0" encoding="UTF-8"?>
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns">
+
+<data key="gFormat">Cyberiada-GraphML</data>
+<key id="dName" for="node" attr.name="name" attr.type="string"/>
+<key id="dData" for="edge" attr.name="data" attr.type="string"/>
+<key id="dData" for="node" attr.name="data" attr.type="string"/>
+<key id="dInitial" for="node" attr.name="initial" attr.type="string"/>
+<key id="dGeometry" for="edge"/>
+<key id="dGeometry" for="node"/>
+<key id="dColor" for="edge"/>
+
+<graph id="G" edgedefault="directed">
+    <node id="">
+        <data key="dName">ArduinoUno</data>
+        <data key="dData">name/ Arduino-Blinker
+description/ Включение и выключение лампочки по таймеру
+        </data>
+    </node>
+
+    <node id="init">
+        <data key="dInitial"></data>
+        <data key="dGeometry" x="311" y="-94"></data>
+    </node>
+    
+    <node id="LED1">
+        <data key="dName">LED1</data>
+        <data key="dData">type/ LED
+name/ Светодиод
+description/ Встроенный в плату светодиод, чтобы им мигать
+pin/ 12
+        </data>
+    </node>
+
+    <node id="timer1">
+        <data key="dName">timer1</data>
+        <data key="dData">type/ Timer
+name/ Светодиод
+description/ Программный таймер.
+        </data>
+    </node>
+
+    <node id="diod1">
+        <data key="dName">Включен</data>
+        <data key="dData">entry/
+LED1.on()
+timer1.start(1000)
+        </data>
+        <data key="dGeometry" x="82" y="57"
+            width="450.0" height="95" />
+    </node>
+
+    <node id="diod2">
+        <data key="dName">Выключен</data>
+        <data key="dData">entry/
+LED1.off()
+timer1.start(1000)
+        </data>
+        <data key="dGeometry" x="81" y="334"
+            width="450" height="95" />
+    </node>
+    
+    <edge source="" target="LED1"></edge>
+    <edge source="" target="timer1"></edge>
+    <edge source="init" target="diod1"></edge>
+    <edge source="diod1" target="diod2">
+        <data key="dData">timer1.timeout/</data>
+        <data key="dColor">#F29727</data>
+        <data key="dGeometry" x="457" y="173"/>
+    </edge>
+
+    <edge source="diod2" target="diod1">
+        <data key="dData">timer1.timeout/</data>
+        <data key="dGeometry" x="16" y="175"/>
+        <data key="dColor">#F24C3D</data>
+    </edge>
+
+</graph>
+</graphml>
+`)).toStrictEqual(
+  {
+    "components": {
+        "LED1": {
+        "id": "LED1", 
+        "parameters": `type/ LED
+name/ Светодиод
+description/ Встроенный в плату светодиод, чтобы им мигать
+pin/ 12`
+      }, 
+      "timer1": {
+        "id": "timer1",
+        "parameters": `type/ Timer
+name/ Светодиод
+description/ Программный таймер.`
+      },
+    }, 
+    "format": "Cyberiada-GraphML", 
+    "initialState": {
+      "id": "init", 
+      "target": "diod1"
+      }, 
+      "keys": [
+        {
+          "attr.name": "name", 
+          "attr.type": "string",
+          "for": "node", "id": "dName"
+        }, 
+        {
+          "attr.name": "data", 
+          "attr.type": "string",
+          "for": "edge",
+          "id": "dData"
+        }, 
+        {
+          "attr.name": "data",
+          "attr.type": "string",
+          "for": "node",
+          "id": "dData"
+        }, {
+          "attr.name": "initial", 
+          "attr.type": "string", 
+          "for": "node", "id": "dInitial"
+        }, 
+        {
+          "attr.name": undefined, 
+          "attr.type": undefined,
+          "for": "edge",
+          "id": "dGeometry"
+        }, 
+        {
+          "attr.name": undefined,
+          "attr.type": undefined,
+          "for": "node",
+          "id": "dGeometry"
+        }, 
+        {
+          "attr.name": undefined,
+          "attr.type": undefined,
+          "for": "edge",
+          "id": "dColor"
+        }
+      ], 
+      "meta": `name/ Arduino-Blinker
+description/ Включение и выключение лампочки по таймеру`, 
+    "platform": "ArduinoUno", 
+    "states": 
+    {
+      "diod1": {
+        "actions": `entry/
+LED1.on()
+timer1.start(1000)`, 
+        "bounds": {
+          "height": 95,
+          "width": 450,
+          "x": 82,
+          "y": 57
+        }, 
+        "name": "Включен", 
+        "unsupportedDataNodes": []
+      }, 
+      "diod2": {
+        "actions": `entry/
+LED1.off()
+timer1.start(1000)`, 
+        "bounds": {
+          "height": 95,
+          "width": 450,
+          "x": 81,
+          "y": 334
+        }, 
+        "name": "Выключен",
+        "unsupportedDataNodes": []
+      }
+    }, 
+    "transitions": [
+      {
+        "source": "init",
+       "target": "diod1", 
+       "unsupportedDataNodes": []
+      }, 
+      {
+        "actions": "timer1.timeout/", 
+        "color": "#F29727", 
+        "position": {"x": 457, "y": 173}, 
+        "source": "diod1", 
+        "target": "diod2",
+        "unsupportedDataNodes": []
+      }, 
+      {
+        "actions": "timer1.timeout/",
+        "color": "#F24C3D",
+        "position": {"x": 16, "y": 175},
+        "source": "diod2",
+        "target": "diod1",
+        "unsupportedDataNodes": []
+      }]
+    });
+})
